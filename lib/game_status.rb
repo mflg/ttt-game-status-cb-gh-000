24cc81d4ea6ae811607e8cde65f6e2924e033091
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-11 12:25:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,6 +18,6 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.detect |combination|
-    board[combination[0]]==board[combination[1]] && board[combination[0]]==board[combination[2]]  
+  WIN_COMBINATIONS.detect do |combination|
+    board[combination[0]]==board[combination[1]] && board[combination[0]]==board[combination[2]]
 end
