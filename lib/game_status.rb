6a9a0f1bd13231cbe1c8c5b262200241f3820b7e
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-11 12:08:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,9 +7,12 @@ end
 
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8],  # Bottom row
-  [0,4,8],  # Diagonal 1
-  [2,4,6]  # Diagonal 2
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,4,8], # Diagonal 1
+  [2,4,6], # Diagonal 2
+  [0,3,6], # Column 1
+  [1,4,7], # Column 2
+  [2,5,8]  # Column 3
   # ETC, an array for each win combination
 ]
